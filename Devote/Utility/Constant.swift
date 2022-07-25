75f11993bbaf326c43a7821df57a39a78561e33e
Light2288 <davide.aliti@gmail.com>
2022-07-26 00:00:33 +0200

    initial set up

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Davide Aliti on 25/07/22.
+//
+
+import Foundation
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
